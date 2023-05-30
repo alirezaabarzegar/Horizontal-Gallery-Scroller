f3e19b4b4d2da3f297fa5d00219750be007a5522
Alireza Barzegar <alireza0barzegar1377@gmail.com>
2023-05-30 21:29:35 +0330

    Initail Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const slider = document.querySelector(".container");
+let isDown = false;
+let startx;
+let scrollToleft;
+
+slider.addEventListener("mousedown", function (e) {
+  isDown = true;
+  slider.classList.add("active");
+  startx = e.pageX - slider.offsetLeft;
+  scrollToleft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseup", function () {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseleave", function () {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", function (e) {
+  if (!isDown) return;
+  e.preventDefault();
+
+  const distanceX = e.pageX - slider.offsetLeft;
+  const walk = distanceX - startx;
+  slider.scrollLeft = scrollToleft - walk;
+});
